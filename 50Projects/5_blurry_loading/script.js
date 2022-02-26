68463e49a47794bbfd546d8fb5f5974fbc8681d3
Jeong yong hoon <jyh.dev@gmail.com>
2022-02-26 16:32:01 +0900

    day5
    1. js 동작에 대한 원리 실험
    
    2. 강의에서 stackoverflow의 scale함수를 사용하여 load 애니메이션을 표현함 참고해보기

---
+++ 50Projects/5_blurry_loading/script.js
@@ -0,0 +1,43 @@
+const bg = document.querySelector('.bg')
+const loadText = document.querySelector('.loading-text')
+
+let load = 0
+
+// function sleep(ms) {
+//     return new Promise((r) => setTimeout(r, ms));
+// }
+//
+// const blurring = async () => {
+//     while (load <= 100) {
+//         console.log(load)
+//         await sleep(30)
+//
+//         bg.style.filter = `blur(${100 - load}px)`
+//         loadText.style.opacity = `${1 - (load / 100)}`
+//
+//         loadText.innerHTML = `${load}%`
+//
+//         load++
+//     }
+// }
+//
+// blurring().then(r => r)
+
+
+const blurring = () => {
+    load++
+    console.log(load)
+    if (load >= 100) clearInterval(interval)
+
+    bg.style.filter = `blur(${100 - load}px)`
+    loadText.style.opacity = `${1 - (load / 100)}`
+
+    loadText.innerHTML = `${load}%`
+}
+
+let interval = setInterval(blurring, 30)
+
+let test = 0
+while (test < 100) {
+    console.log(`a${test++}`)
+}
