5ddb4f70d4f05ec0b87cb85f983ea40caa1cb825
Jeong yong hoon <jyh.dev@gmail.com>
2022-02-28 14:57:24 +0900

    day6
    script 학습 필요

--- 50Projects/6_scroll_animation/script.js
+++ 50Projects/6_scroll_animation/script.js
@@ -1,8 +1,21 @@
 const boxes = document.querySelectorAll('.box')
 
 const checkBoxes = () => {
-    console.log('123')
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
 }
 
 window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
 
