788de15cd5e852821356eb1372b7f0c7b2efbfac
Jeong yong hoon <jyh.dev@gmail.com>
2022-03-20 13:53:07 +0900

    Day9

---
+++ 50Projects/9_sound_board/script.js
@@ -0,0 +1,26 @@
+const sounds = ['1', '2', '3', '4', '5', '6']
+
+sounds.forEach(sound => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+
+    btn.innerText = sound
+
+    btn.addEventListener("click", () => {
+        stopSongs();
+
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById(("buttons")).appendChild(btn);
+})
+
+const stopSongs = () => {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+        song.pause();
+
+        // mp3 시간이 0으로 돌아감.. 이건 무슨 파라미터지..?
+        song.currentTime = 0;
+    })
+}
