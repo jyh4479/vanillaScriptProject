92a6bca0a5e7e95af7c512229b16ec51cdc42d8c
Jeong yong hoon <jyh.dev@gmail.com>
2022-04-24 22:31:06 +0900

    day14
    오랜만에 써본 바닐라 js....
    또 까먹었네~ 다시 꾸준히 시작하자

---
+++ 50Projects/14_animated_navigation/script.js
@@ -0,0 +1,4 @@
+const toggleButton = document.getElementById("toggle");
+const navigationBar = document.getElementById("nav");
+
+toggleButton.addEventListener("click", () => navigationBar.classList.toggle("active"))
