e6395c520656b2d5e53b656bb4f722ba4502152a
Jeong yong hoon <jyh.dev@gmail.com>
2022-04-27 22:20:01 +0900

    day15
    js 내부 동작에 대한 개인적인 생각

--- 50Projects/15_increment_counter/script.js
+++ 50Projects/15_increment_counter/script.js
@@ -13,6 +13,8 @@ counters.forEach(counter => {
 
         if (c < target) {
             counter.innerText = `${Math.ceil(c + increment)}`
+            // 여기서 재귀로 함수를 호출 하는데 다른 언어들과 마찬가지로
+            // 뎁스가 깊어지는건지 stack -> queue -> stack 처리로 반복문과 같은 효과를 내는건지 내부적으로 알아볼 필요가 있다.
             setTimeout(updateCounter, 1);
         }
     }
