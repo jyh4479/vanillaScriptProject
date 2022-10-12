a00a341d204c0531a72f739e66bd327d3df6c6d5
yonghoon_jeong <yonghoon_jeong@tmax.co.kr>
2022-10-12 16:07:35 +0900

    commit test

--- 50Projects/0_work_space/script.js
+++ 50Projects/0_work_space/script.js
@@ -18,3 +18,23 @@ const b = () => {
 }
 
 main(a, b);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function marry(man, woman) {
+    woman.husband = man;
+    man.wife = woman;
+
+    return {
+        father: man,
+        mother: woman
+    }
+}
+
+let family = marry({
+    name: "Jhon"
+}, {
+    name: "Ann"
+})
+
+console.log(family)
--- 50Projects/8_form_input_wave/script.js
+++ 50Projects/8_form_input_wave/script.js
@@ -1,5 +1,7 @@
 const labels = document.querySelectorAll('.form-control label')
 
+const testView = []
+
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
@@ -7,4 +9,15 @@ labels.forEach(label => {
         .map((letter, idx) => `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`)
         // 문자로 반환하기 위한 join
         .join('')
+
+    testView.push(
+        label.innerText
+            .split('')
+            // index를 활용해 각각 delay 적용
+            .map((letter, idx) => `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`)
+            // 문자로 반환하기 위한 join
+            .join('')
+    )
 })
+
+console.log(testView)
